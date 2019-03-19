b7a80b7ca2ffebf156b052652a304be2613ea20e
Jonas De Kegel <jonas@jlss.eu>
2019-03-19 13:01:46 +0100

    Expose Tag.vue
    Allows for a custom (renderless) component to cleanly reuse the tag

--- bulma/index.js
+++ bulma/index.js
@@ -1,4 +1,5 @@
 import VueSelect from '../src/bulma/VueSelect.vue';
 import EnsoSelect from '../src/bulma/EnsoSelect.vue';
+import Tag from '../src/bulma/Tag.vue';
 
-export { VueSelect, EnsoSelect };
+export { VueSelect, EnsoSelect, Tag };
