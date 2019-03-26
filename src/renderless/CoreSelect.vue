c0bc957ae49ed2dab85e2246892f6c98e10c08c4
mishu <mishu@xtelecom.ro>
2019-03-26 16:44:34 +0200

    fixes objects with null selection

--- src/renderless/CoreSelect.vue
+++ src/renderless/CoreSelect.vue
@@ -217,7 +217,7 @@ export default {
                 : null;
         },
         valueMatchesOption(value, option) {
-            return this.objects
+            return value !== null && this.objects
                 ? value[this.trackBy] === option[this.trackBy]
                 : value === option[this.trackBy];
         },
