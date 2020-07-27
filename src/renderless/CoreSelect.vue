4eb8ff0f0789f2df19ff6be766aa14c38bd64425
mishu <mishu@xtelecom.ro>
2020-07-27 11:56:48 +0300

    makes dropdown available when no options but using taggable

--- src/renderless/CoreSelect.vue
+++ src/renderless/CoreSelect.vue
@@ -162,7 +162,7 @@ export default {
         },
         dropdownDisabled() {
             return this.readonly || this.disabled
-                || !this.hasOptions && !this.query;
+                || !this.hasOptions && !this.query && !this.taggable;
         },
     },
 
